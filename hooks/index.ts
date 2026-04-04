819b9f7a18871af1c9e6c77e35598925e9ab0d82
Projectokart <ptdevkaushik104@gmail.com>
2026-04-04 03:53:05 -0700

    Delete hooks directory

--- hooks/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./useExperiments";
-export * from "./useProjects";
-export * from "./useBlog";
-export * from "./useMaterials";
-export { useIsMobile } from "./use-mobile";
-export { useToast } from "./use-toast";
